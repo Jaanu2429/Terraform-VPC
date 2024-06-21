5a50700178287f1bd5af20c84790c3bc5eaa26ed
Jaanu2429 <150994702+Jaanu2429@users.noreply.github.com>
2024-06-21 16:34:51 +0530

    Update outputs.tf

--- Terraform-VPC/modules/vpc/outputs.tf
+++ Terraform-VPC/modules/vpc/outputs.tf
@@ -3,9 +3,25 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public_subnets.*.id
+  value = aws_subnet.public_subnets[*].id
 }
 
 output "private_subnet_ids" {
-  value = aws_subnet.private_subnets.*.id
+  value = aws_subnet.private_subnets[*].id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat_gw[*].id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public_rt.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private_rt[*].id
 }
