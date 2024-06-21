52e3dc79b34f5a4395bdaae14d2877f0f7e74ab7
Jaanu2429 <150994702+Jaanu2429@users.noreply.github.com>
2024-06-21 20:05:53 +0530

    Update variables.tf

--- Terraform-VPC/variables.tf
+++ Terraform-VPC/variables.tf
@@ -1,9 +1,31 @@
-variable "vpc_cidr" {
-  description = "VPC CIDR Range"
-  type = string
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  type        = string
+  default     = "us-west-2"
 }
 
-variable "subnet_cidr" {
-    description = "Subnet CIDRS"
-    type = list(string)
+variable "vpc_cidr" {
+  description = "VPC CIDR Range"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public Subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private Subnet CIDRs"
+  type        = list(string)
+}
+
+variable "subnet_names" {
+  description = "Subnet names"
+  type        = list(string)
+}
+
+variable "use_nat_gateway" {
+  description = "Use NAT Gateway for private subnets (true/false)"
+  type        = bool
+  default     = true
 }
