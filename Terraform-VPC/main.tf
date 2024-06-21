d90f382cfc0d316ef610ac89a5229b4f332a1893
Jaanu2429 <150994702+Jaanu2429@users.noreply.github.com>
2024-06-21 20:04:38 +0530

    Update main.tf

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -1,7 +1,10 @@
 module "vpc" {
-  source = "./modules/vpc"
-  vpc_cidr = var.vpc_cidr
-  subnet_cidr = var.subnet_cidr
+  source              = "./modules/vpc"
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidrs = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  subnet_names        = var.subnet_names
+  use_nat_gateway     = var.use_nat_gateway
 }
 
 module "sg" {
