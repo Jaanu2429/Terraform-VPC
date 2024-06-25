8c1c192e35b00a61e0220d17008eb94ab4e039ee
Jaanu2429 <150994702+Jaanu2429@users.noreply.github.com>
2024-06-25 12:36:06 +0530

    Update variables.tf

--- Terraform-VPC/modules/vpc/variables.tf
+++ Terraform-VPC/modules/vpc/variables.tf
@@ -6,22 +6,23 @@ variable "vpc_cidr" {
 variable "public_subnet_cidrs" {
   description = "Public Subnet CIDRs"
   type = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "Private Subnet CIDRs"
   type = list(string)
-  default = ["10.0.3.0/24"]
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "subnet_names" {
   description = "Subnet names"
   type = list(string)
-  default = ["PublicSubnet1", "PublicSubnet2", "PrivateSubnet1"]
+  default = ["PublicSubnet1", "PublicSubnet2", "PrivateSubnet1", "PrivateSubnet2"]
 }
 
 variable "use_nat_gateway" {
   description = "Use NAT Gateway for private subnets (true/false)"
   type = bool
-  default = false # Set to false as NAT Gateway is not wanted
+  default = true
 }
