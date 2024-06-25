41acdcffd5207133c036bd58641b4e6596862bc5
Jaanu2429 <150994702+Jaanu2429@users.noreply.github.com>
2024-06-25 11:33:44 +0530

    Update variables.tf

--- Terraform-VPC/modules/vpc/variables.tf
+++ Terraform-VPC/modules/vpc/variables.tf
@@ -11,11 +11,13 @@ variable "public_subnet_cidrs" {
 variable "private_subnet_cidrs" {
   description = "Private Subnet CIDRs"
   type = list(string)
+  default = ["10.0.3.0/24"]
 }
 
 variable "subnet_names" {
   description = "Subnet names"
   type = list(string)
+  default = ["PublicSubnet1", "PublicSubnet2", "PrivateSubnet1"]
 }
 
 variable "use_nat_gateway" {
